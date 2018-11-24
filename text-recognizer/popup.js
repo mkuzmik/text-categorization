373a1db0eafa027713bb5ac51f998e1b43227aed
Mateusz Kuzmik <mateusz.kuzmik1@gmail.com>
2018-11-24 13:48:31 +0100

    Form style updated

--- text-recognizer/popup.js
+++ text-recognizer/popup.js
@@ -2,6 +2,10 @@ $(function(){
     $('#paste').click(function(){pasteSelection();});
 });
 
+$(function(){
+    $('#recognize').click(function(){recognize();});
+});
+
 function pasteSelection() {
     chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT},
         function(tab) {
@@ -9,6 +13,12 @@ function pasteSelection() {
                 function(response){
                     var text = document.getElementById('text');
                     text.innerHTML = response.data;
+                    M.textareaAutoResize(text);
                 });
         });
+}
+
+function recognize() {
+    var result = document.getElementById('result');
+    result.value = 'result here';
 }
